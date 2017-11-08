4679160b41a49a9cd9c9d1bef6d941214b98603f
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-11-08 10:56:23 +0800

    Split project path

---
+++ server/config.js
@@ -0,0 +1,5 @@
+const projectPath = '/home/y/project/2017/06/yesdemo/src/thgn/';
+
+module.exports = {
+    projectPath,
+};
--- server/routes/file.js
+++ server/routes/file.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const config = require('../config');
 const router = express.Router();
 
-const basePath = '/home/y/project/2017/06/yesdemo/src/thgn/';
-/* GET users listing. */
 router.get('/', (req, res, next) => {
-    console.log(req.query.path);
-    // if (!req.query.path) {
-    //     res.send('没有文件名');
-    //     return;
-    // }
-    const filePath = path.join(basePath, req.query.path);
-    // const stat = fs.statSync(filePath);
-    //
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/plain',
-    //     'Content-Length': stat.size,
-    // });
-    //
-    // const readStream = fs.createReadStream(filePath);
-    // readStream.pipe(res);
-
+    const filePath = path.join(config.projectPath, req.query.path);
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             res.json({
@@ -37,7 +20,6 @@ router.get('/', (req, res, next) => {
     });
 });
 
-/* GET users listing. */
 router.put('/', (req, res, next) => {
     const filePath = path.join(basePath, req.body.path);
     const code = req.body.code;
